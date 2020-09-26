58fd5fdd625dc4b41005b9ab55f56e6ddb144843
edsondesennajunior <edsonsjr62@gmail.com>
2020-09-26 16:04:36 -0300

    Adding method to toggle done and to create a new one

--- assets/js/HtmlService.js
+++ assets/js/HtmlService.js
@@ -1,18 +1,69 @@
+const DONE_CSS_CLASS = 'done';
+
 export default class HtmlService {
-    constructor() {
-        console.log('Service created successfully!');
+    constructor(todoService) {
+        this.todoService = todoService;
         this.bindFromEvent();
+        this.listTasks();
     }
 
     bindFromEvent() {
         const form = document.querySelector('form');
         form.addEventListener('submit', event => {
             event.preventDefault();
+            this.createTask(form.item.value);
             console.log(form.item.value);
             form.reset();
         })
     }
 
+    async createTask(description) {
+        let task = {description, done: false};
+        const createdTaskId = await this.todoService.save(task);
+        task['id'] = createdTaskId;
+        this.addToHtmlList(task);
+    }
+
+    async saveTask(taskId, isDone) {
+        const task = await this.todoService.get(taskId);
+        task.done = isDone;
+        this.todoService.save(task);
+    }
+
+    async listTasks() {
+        const tasks = await this.todoService.getAll();
+        tasks.map((task) => this.addToHtmlList(task));
+        console.log(tasks);
+    }
+
+    toggleTask(li) {
+        const taskId = +li.getAttribute('data-item-id');
+        li.classList.toggle(DONE_CSS_CLASS);
+        const isDone = li.classList.contains(DONE_CSS_CLASS);
+    }
+
+    addToHtmlList(task) {
+        const ul = document.querySelector('ul');
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+        const button = document.createElement('button');
+
+        li.setAttribute('data-item', task.id);
+        li.addEventListener('click', () => this.toggleTask(li))
+
+        span.textContent = task.description;
+        button.textContent = 'x';
+
+        if(task.done) {
+            li.classList.add(DONE_CSS_CLASS);
+        }
+
+        li.appendChild(span);
+        li.appendChild(button);
+
+        ul.appendChild(li);
+
+    }
 
 
 
--- assets/js/TodoService.js
+++ assets/js/TodoService.js
@@ -1,47 +1,54 @@
-const db = new Dexie('todoDB');
+let db;
 
-db.version(1).stores({
-    tasks: '++id, description, done'
-});
+export default class TodoService {
 
-db.on('populate', async () => {
-    console.log('It runs only once!');
-    await db.tasks.bulkPut([
-        {
-            description: 'Learn JS',
-            done: false
-        },
-        {
-            description: 'Learn Python',
-            done: false
-        },
-        {
-            description: 'Learn C++',
-            done: false
-        },
-        {
-            description: 'Learn TS',
-            done: false
-        },
-    ])
-})
+    constructor() {
+        this.initializeDB();
+    }
 
-async function list() {
-    db.tasks.each(task => console.log(task));
-    const taskTakeScript = await db.tasks.get(4);
-    taskTakeScript.done = true;
-    db.tasks.put(taskTakeScript);
-    const tasksDone = await db.tasks
-        .where('description')
-        .equals('Learn TS').first();
-    console.log('Query', tasksDone);
+    initializeDB() {
+        db = new Dexie('todoDB');
 
+        db.version(1).stores({
+            tasks: '++id, description, done'
+        });
+        
+        db.on('populate', async () => {
+            console.log('It runs only once!');
+            await db.tasks.bulkPut([
+                {
+                    description: 'Learn JS',
+                    done: false
+                },
+                {
+                    description: 'Learn Python',
+                    done: false
+                },
+                {
+                    description: 'Learn C++',
+                    done: false
+                },
+                {
+                    description: 'Learn TS',
+                    done: false
+                },
+            ])
+        })
+    }
+
+    getAll() {
+        return db.tasks.toArray();
+    }
+
+    get(id) {
+        return db.tasks.get(id);
+    }
+
+    save(task) {
+        return db.tasks.put(task);
+    }
+
+    delete(id) {
+        return db.tasks.delete(id);
+    }
 }
-
-list();
-
-// export default class TodoService {
-//     constructor() {
-
-//     }
-// }
